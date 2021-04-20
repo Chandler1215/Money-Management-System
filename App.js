37a2c5bf2287c9e2b495337b7cddbd2a86a7db4e
Bozîeac Artur <39851804+Chandler1215@users.noreply.github.com>
2021-04-21 01:16:30 +0300

    Using boilerplate template

--- App.js
+++ App.js
@@ -1,150 +1,57 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Linking } from 'react-native';
-import { Footer, Button, Title, Container, Content } from 'native-base';
-import { Header } from 'react-native-elements';
-import { Ionicons } from '@expo/vector-icons';
-import {
-  SafeAreaView,
-  SafeAreaProvider,
-  SafeAreaInsetsContext,
-  useSafeAreaInsets,
-  initialWindowMetrics,
-} from 'react-native-safe-area-context';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { ThemeProvider } from "@shopify/restyle";
+import { createStackNavigator } from "@react-navigation/stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Provider } from "react-redux";
+import "react-native-gesture-handler";
 
-const Category = () => {
-  const categoryClickedHandler = () => {
-    console.log('You have been clicked a button!');
-    // do something
-  };
+import store from "./store";
 
+/* Stacks */
+import { AllAppsStackNavigator } from "./src/components/AllApps";
+import { ExpenseNavigator } from "./src/expense__tracker";
+import { EmailNavigator } from "./src/gmail";
+
+import { LoadAssets, theme } from "./src/components";
+
+import { Bitcoin, Dash, Eth, Lite, Tether } from "./assets/images";
+
+const assets = [Bitcoin, Dash, Eth, Lite, Tether];
+
+const fonts = {
+  SFBOLD: require("./assets/fonts/SF-Pro-Text-Bold.otf"),
+  SFREGULAR: require("./assets/fonts/SF-Pro-Text-Regular.otf"),
+  SFSEMI: require("./assets/fonts/SF-Pro-Text-Semibold.otf"),
+  RBlack: require("./assets/fonts/Rubik/Rubik-Black.ttf"),
+  RRegular: require("./assets/fonts/Rubik/Rubik-Regular.ttf"),
+  RMedium: require("./assets/fonts/Rubik/Rubik-Medium.ttf"),
+};
+
+const AppStack = createStackNavigator();
+
+export default function App() {
   return (
-    <TouchableOpacity
-      onPress={categoryClickedHandler}
-      style={styles.categoryButton}>
-      <Text>Item</Text>
-      <Text>$0</Text>
-    </TouchableOpacity>
+    <Provider {...{ store }}>
+      <ThemeProvider {...{ theme }}>
+        <LoadAssets {...{ assets, fonts }}>
+          <SafeAreaProvider>
+            <AppStack.Navigator headerMode="none" initialRouteName="AllApps">
+              <AppStack.Screen
+                name="AllApps"
+                component={AllAppsStackNavigator}
+              />
+              <AppStack.Screen
+                name="ExpenseTracker"
+                component={ExpenseNavigator}
+              />
+
+              <AppStack.Screen name="Email" component={EmailNavigator} />
+            </AppStack.Navigator>
+          </SafeAreaProvider>
+        </LoadAssets>
+        <StatusBar />
+      </ThemeProvider>
+    </Provider>
   );
 }
-
-const MainButton = () => {
-  const buttonClickedHandler = () => {
-    console.log('You have been clicked a button!');
-    // do something
-  };
-
-  return (
-    <View title="ExpensesCenterText" style={styles.centered}>
-        <TouchableOpacity
-          onPress={buttonClickedHandler}
-          style={styles.totalExpensesButton}>
-          <Text style={styles.totalExpensesText}>Expenses</Text>
-          <Text style={styles.totalIncomeValue}>$750&#9650;</Text>
-          <Text style={styles.totalExpenseValue}>$600&#9660;</Text>
-        </TouchableOpacity>
-    </View>
-  );
-}
-
-function App() {
-  const buttonClickedHandler = () => {
-    console.log('You have been clicked a button!');
-    // do something
-  };
-
-  return (
-<SafeAreaProvider>
-  <Container>
-      <SafeAreaView>
-      <Header
-        leftComponent=<Ionicons name="menu" size={32} color="white" />
-        centerComponent={{ text: 'Big Money', style: { color: '#fff', fontSize: 20, fontWeight: 700 } }}
-      />
-      </SafeAreaView>
-  
-      <Content style={styles.centered}>
-
-        <View style={styles.categoryButtonContainer}>
-          <Category/>
-          <Category/>
-          <Category/>
-          <Category/>
-        </View>
-
-        <View>
-          <MainButton/>
-        </View>
-        
-        <View style={styles.categoryButtonContainer}>
-          <Category/>
-          <Category/>
-          <Category/>
-          <Category/>
-        </View>
-
-      </Content>
-
-      <Footer>
-        <Button transparent>
-            <Ionicons name="home" size={32} color="white" />
-        </Button>
-      </Footer>
-      
-    </Container>
-    </SafeAreaProvider>
-  );
-}
-
-export default App;
-
-/// Just some styles
-const styles = StyleSheet.create({
-  centered: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  categoryButtonContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap'
-  },
-  categoryButton: {
-    width: 70,
-    height: 70,
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin: 5,
-    borderRadius: 100,
-    borderWidth: 6,
-    borderColor: '#d4defa',
-    backgroundColor: '#e1e8fc'
-  },
-  totalExpensesButton: {
-    margin: 20,
-    width: 150,
-    height: 150,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 10,
-    borderRadius: 100,
-    borderWidth: 7,
-    borderColor: '#03a5fc'
-  },
-  totalExpensesText: {
-    fontSize: 16,
-    fontWeight: 600,
-    padding: 5
-  },
-  totalExpenseValue: {
-    fontSize: 14,
-    fontWeight: 600,
-    color: '#e80707',
-    padding: 1
-  },
-  totalIncomeValue: {
-    fontSize: 14,
-    fontWeight: 600,
-    color: '#41b812',
-    padding: 1
-  }
-});
